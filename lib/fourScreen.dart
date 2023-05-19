0eeb96577143f97e7e90526f8fe8bbc6cd53445e
이택 <f_cd_w@naver.com>
2023-05-19 16:29:53 +0900

    Initial commit

---
+++ lib/fourScreen.dart
@@ -0,0 +1,192 @@
+import 'dart:collection';
+import 'package:fimga_calendar/sixScreen.dart';
+import 'sevenScreen.dart';
+import 'fiveScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:fimga_calendar/fiveScreen.dart';
+
+
+class fourScreen extends StatelessWidget {
+  const fourScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: calendar(),
+    );
+  }
+}
+
+
+class calendar extends StatefulWidget {
+  const calendar({Key? key}) : super(key: key);
+
+  @override
+  State<calendar> createState() => _calendarState();
+}
+
+
+class _calendarState extends State<calendar> {
+  List alist = List<int>.filled(31,0);
+  DateTime? _selectedDay;
+  DateTime _focusedDay = DateTime.now();
+  Map<DateTime, List<sb>> events={
+    DateTime.utc(2023,05,01):[sb()],
+    DateTime.utc(2023,05,02):[sb()],
+  };
+
+  List<sb> _getEventsForDay(DateTime day){
+    return events[day] ??[];
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Table calendar"),
+      ),
+      body:content(),
+    );
+  }
+  Widget content(){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text("My Calendar"),
+        Container(
+          width: 800,
+          height: 430,
+          child: TableCalendar(
+            locale: 'ko-KR',
+            firstDay: DateTime.utc(2022,05,01),
+            lastDay: DateTime.utc(2023,05,31),
+            focusedDay: DateTime.now(),
+            headerStyle: HeaderStyle(
+              titleCentered: true,
+              formatButtonVisible: false,
+            ),
+            daysOfWeekHeight: 20, //한국어로 바꾸니까 글자 안맞아서 일~토 높이 20으로 증가
+            // calendarStyle: CalendarStyle(
+            //     cellMargin: const EdgeInsets.all(7.0),
+            //     cellPadding: const EdgeInsets.only(bottom:5.0)
+            // ),
+            selectedDayPredicate: (day){
+              return isSameDay(_selectedDay,day);
+            },
+            onDaySelected: (selectedDay,focusedDay){
+              setState(() {
+                _selectedDay=selectedDay;
+                _focusedDay=focusedDay;
+              });
+            },
+            eventLoader: _getEventsForDay,
+            calendarBuilders: CalendarBuilders(
+              defaultBuilder: (context, dateTime, _) {
+                return CalendarCellBuilder(context, dateTime, _, 0);
+              },
+              todayBuilder: (context, dateTime, _) {
+                return CalendarCellBuilder(context, dateTime, _, 1);
+              },
+              selectedBuilder: (context, dateTime, _) {
+                return CalendarCellBuilder(context, dateTime, _, 2);
+              },
+            ),
+          ),
+          // child:CalendarStyle(
+          //
+          // )
+        ),
+        TextButton(onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>fiveScreen()));
+        },
+            child:Container(
+                width: 380,
+                height: 200,
+                decoration: BoxDecoration(
+                    color: Color(0xffE7E3D1),
+                    border: Border.all(width: 2,color: Color(0xffE7E3D1)),
+                    borderRadius: BorderRadius.circular(20)
+                ),
+                child:Column(
+                  children: [
+                    Container(
+                      child:ClipRRect(
+                        child:Image.asset("imageFile/뼈다구.png",width: 60,height: 80,),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      margin: EdgeInsets.fromLTRB(0, 0, 300, 0),
+                    )
+                  ],
+                )
+            ),
+        ),
+        Container(
+          width: double.infinity,
+          height: 80,
+          color: Color(0xffFCF5B6),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Image.asset("imageFile/calendar.PNG"),
+              TextButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>sevenScreen()));},
+                child:Image.asset("imageFile/monglogo.PNG"),
+          ),
+              TextButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>sixScreen()));
+              }, child:Image.asset("imageFile/tong.PNG"),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+Widget CalendarCellBuilder(BuildContext context, DateTime dateTime, _, int type){
+  String date =dateTime.day.toString();
+  String month = dateTime.month.toString();
+  return Container(
+      padding: EdgeInsets.all(5),
+      child:Column(
+          children:[
+            Text(
+              date,
+              style: TextStyle(fontSize: 8,color: Color(0xffDBD09E)),
+            ),
+            // Image.asset("imageFile/d.png")
+            (date=='2' && month=='5')?
+            Container(
+                child: Image.asset("imageFile/d.png",width: 30,height: 30,),
+                // padding: EdgeInsets.only(top: 1, bottom: 1),
+                width: MediaQuery.of(context).size.width,
+                height:30,
+                decoration: BoxDecoration(
+                    shape:BoxShape.circle,
+                    color: Color(0xffE7E3D1)
+                )
+            ):
+            Container(
+              // padding: EdgeInsets.only(top: 1, bottom: 1),
+              width: MediaQuery.of(context).size.width,
+              height:32,
+              decoration: BoxDecoration(
+                shape:BoxShape.circle,
+                color: Color(0xffE7E3D1),
+              ),
+            )
+          ]
+      )
+  );
+}
+class sb{
+  SizedBox sizebox=SizedBox(
+    width: 40,
+    height: 40,
+    child: Container(color: Colors.blue,),
+  );
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'oneScreen.dart';
+import 'twoScreen.dart';
+import 'threeScreen.dart';
+import 'fourScreen.dart';
+import 'fiveScreen.dart';
+import 'sixScreen.dart';
+import 'sevenScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() {
+  initializeDateFormatting().then((_) => runApp(MaterialApp(home: myApp(),)));
+  }
+
+class myApp extends StatefulWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  State<myApp> createState() => _myAppState();
+}
+
+class _myAppState extends State<myApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/':(context)=>oneScreen(),
+        '/twoScreen':(context)=>twoScreen(),
+        '/threeScreen':(context)=>threeScreen(),
+        '/fourScreen':(context)=>fourScreen(),
+        // '/fiveScreen':(content)=>fiveScreen(),
+        // '/sixScreen':(content)=>sixScreen(),
+        // '/sevenScreen':(content)=>sevenScreeen()
+      },
+    );
+  }
+}
